d629c5891d8d6169004edcb168a2214bb079d599
Nisheet Sharma <nisheethsharma@gmail.com>
2019-04-28 22:29:25 +0530

    added comments

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+# LOAD NVM
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+# USE NODE V6.9.6 & START SERVING
 nvm use 6.9.5 && npm start
